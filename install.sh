75d8d2c5796bd35ceb5f665682507e2e26a4f5ee
V-EnZo <aziziasl00@gmail.com>
2026-07-02 02:17:05 +0330

    Update OpenVPN installation script URL

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O openvpn-install.sh https://raw.githubusercontent.com/My-VEnZo/REPO/main/openvpn-install.sh
+wget -O openvpn-install.sh https://raw.githubusercontent.com/My-VEnZo/ovpanel/main/openvpn-install.sh
 
 chmod +x openvpn-install.sh
 
